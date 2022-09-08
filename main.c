586dc6c1c68a1518db564c357f81084b7ef7c2f9
Osvaldo <35927834+iosvaldo@users.noreply.github.com>
2022-09-08 02:40:32 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+// C Program to implement
+// the above approach
+#include <stdio.h>
+  
+// Driver code
+int main()
+{
+    char word[100];
+  
+    // word is treated as a pointer
+    // to the first element of array
+    scanf("%s", word);
+    printf("Output : %s", word);
+    return 0;
+}
